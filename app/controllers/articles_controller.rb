a191972ff20ea6e59c6f468930100f8c91a6d441
gdumani <gdumani@gmail.com>
2020-09-29 16:30:37 -0600

    add delete and edit functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,4 +18,17 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)    
   end
+
+  def edit
+    @article = Article.find(params[:id])
+ 
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+ 
+    redirect_to articles_path
+  end
+
 end
