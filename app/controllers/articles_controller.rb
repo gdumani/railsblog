2f67e218d9d4748c9454648a50d08e13f07d1bd4
Miguel Uzcátegui <migueluzcategui09@gmail.com>
2020-09-30 15:42:45 -0400

    Add flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)    
   end
 
@@ -27,13 +30,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
 
+    flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
- 
+    
+    flash.notice = "Article '#{@article.title}' Deleted!"
+    
     redirect_to articles_path
   end
 
