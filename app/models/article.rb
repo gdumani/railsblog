0dae31fd3e3f46324795d635bb3243077014d1dc
gdumani <gdumani@gmail.com>
2020-10-01 09:40:47 -0600

    added imagemagick and image attachments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
-  has_many :comments
-  has_many :taggings
+  has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
-  has_attached_file :image
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_many :attachments, dependent: :destroy
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   
